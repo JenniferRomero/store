32f6a0c1351f3251ee201ce3534c3522d41d48d7
Jennifer <jennifer@experimentality.co>
2021-02-02 14:30:14 -0500

    cambio package

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/project-name'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/store'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/project-name' }
-  );
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/store/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
